43208891e61f9762b92da035cd2823792dd5b772
Evgeniy <46program@gmail.com>
2023-10-17 21:35:19 +0300

    lesson 11

---
+++ src/main/java/org/itstep/lesson11/practice/MethodsTask5.java
@@ -0,0 +1,19 @@
+package org.itstep.lesson11.practice;
+
+public class MethodsTask5 {
+    //Напишіть метод, який повертає суму чисел у вказаному діапазоні.
+    // Межі діапазону передаються як параметри.
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 50;
+        System.out.println(summa(a, b));
+    }
+
+    public static int summa(int a, int b) {
+        int sum = 0;
+        for (int i = a; i < b; i++) {
+            sum = sum + i;
+        } return
+                sum;
+    }
+}
