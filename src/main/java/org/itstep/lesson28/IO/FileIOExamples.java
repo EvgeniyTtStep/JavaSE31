8ef8590b88def6ad2443dec21ad364bcd9076159
Evgeniy <46program@gmail.com>
2023-11-25 21:32:15 +0200

    lesson 28 io example task3

--- src/main/java/org/itstep/lesson28/IO/FileIOExamples.java
+++ src/main/java/org/itstep/lesson28/IO/FileIOExamples.java
@@ -1,25 +1,104 @@
 package org.itstep.lesson28.IO;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class FileIOExamples {
     public static void main(String[] args) {
-        inputOutputEx();
-        inputEx();
-        outEx();
+        //inputOutputEx();
+        //inputEx();
+        //outEx();
+        //bufferedInOutStream();
+        //charReaderWriter();
+        //charReaderWriterBuf();
+        //scanner();
+        streamUrl();
     }
-    private static void inputEx() {
 
-        try {
-            InputStream in = new FileInputStream("index.txt");
-            byte[]buf = new byte[5];
-            int read;
-            while ((read = in.read(buf)) != -1){
-                String str = new String(buf, 0, read, StandardCharsets.UTF_8);
+    private static void streamUrl() {
+        try(InputStream in = new URL("https://www.google.com/").openStream();
+        OutputStream out = new FileOutputStream("google.html")) {
+
+            byte[] buf = new byte[1024];
+            int length;
+            while ((length = in.read(buf)) != -1) {
+                out.write(buf, 0, length);
+            }
+
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void scanner() {
+        try (Scanner scanner = new Scanner(new FileInputStream("index.txt"))) {
+            while (scanner.hasNext()) {
+                String string = scanner.next();
+                System.out.print(string + " ");
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private static void charReaderWriterBuf() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("index.txt"));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("out.txt"))) {
+            String string = reader.readLine();
+            writer.write(string);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private static void charReaderWriter() {
+        try (Reader reader = new FileReader("index.txt");
+             Writer writer = new FileWriter("out.txt")) {
+            char[] chars = new char[256];
+            int length;
+            while ((length = reader.read(chars)) != -1) {
+                writer.write(chars, 0, length);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void bufferedInOutStream() {
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream("index.txt"));
+             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("out.txt"))) {
+            System.out.println("available = " + in.available());
+            byte[] buf = new byte[in.available()];
+            int length = in.read(buf);
+            out.write(buf, 0, length);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    private static void inputEx() {
+        try (InputStream in = new FileInputStream("index.txt");) {
+            byte[] buf = new byte[5];
+            int length;
+            while ((length = in.read(buf)) != -1) {
+                String str = new String(buf, 0, length, StandardCharsets.UTF_8);
                 System.out.print(str);
             }
 
@@ -28,16 +107,35 @@ public class FileIOExamples {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 
 
     private static void outEx() {
+        try (OutputStream out = new FileOutputStream("out.txt")) {
+            String str = "Feed back out stream";
+            out.write(str.getBytes());
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
-
     private static void inputOutputEx() {
+        try (InputStream in = new FileInputStream("index.txt");
+             OutputStream out = new FileOutputStream("out.txt")) {
+            byte[] buf = new byte[5];
+            int length;
+            while ((length = in.read(buf)) != -1) {
+                out.write(buf, 0, length);
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/java/org/itstep/lesson28/IO/practice/FileTask3.java
@@ -0,0 +1,53 @@
+package org.itstep.lesson28.IO.practice;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+import static org.openqa.selenium.remote.http.Contents.reader;
+
+public class FileTask3 {
+    //Завдання 3:
+    //Користувач вводить з клавіатури шлях до файлу та слово для
+    //пошуку. Після роботи програми на екрані відображається
+    //кількість разів, скільки слово зустрічається у файлі.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть назву файлу");
+        String fileName = scanner.nextLine();
+        System.out.println("Введіть слово для пошуку");
+        String word = scanner.nextLine();
+
+        String file = task3(fileName);
+
+        int index = 0;
+        int count = 0;
+
+        while (index != -1) {
+            index = file.indexOf(word, index + 1);
+            count++;
+        }
+
+        System.out.println("Слово " + word + "зустрічається " + count + " разів");
+
+
+    }
+
+    private static String task3(String fileName) {
+
+        try (BufferedReader read = new BufferedReader(new FileReader(fileName))) {
+            char[] buf = new char[1024];
+            int length = read.read(buf);
+            return new String(buf, 0, length);
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
