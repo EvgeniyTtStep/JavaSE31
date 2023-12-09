49f2d7cb1ab5c053449cdac9b5c8e6cbfe8ca1de
Evgeniy <46program@gmail.com>
2023-12-09 20:34:21 +0200

    lesson 33 Junit5 annotations

--- src/test/org/itstep/Lesson33/MathematicTest.java
+++ src/test/org/itstep/Lesson33/MathematicTest.java
@@ -1,13 +1,14 @@
 package org.itstep.Lesson33;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathematicTest {
+
 
     Mathematic mathematic;
 
@@ -22,16 +23,48 @@ class MathematicTest {
         System.out.println("End test");
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "10, -10, 0",
+            "20, 10, 30",
+            "15, 15, 30"
+    })
+    void sumPositiveParameterizedTest(String first, String second, String third) {
+        int actualRes = Mathematic.sum(Integer.parseInt(first),Integer.parseInt(second) );
+        int expectedRes = Integer.parseInt(third);
+        Assertions.assertEquals(expectedRes, actualRes);
+    }
 
-    @Test
+
+    @ParameterizedTest
+    @ValueSource(ints = {10, -10, 20 })
+    void sumPositiveParameterizedTest(int param) {
+        int actualRes = Mathematic.sum(10, param);
+        int expectedRes = 30;
+        Assertions.assertEquals(expectedRes, actualRes);
+    }
+
+
+
+    @RepeatedTest(10)
+    void sumPositiveRepeatedTest() {
+        int actualRes = Mathematic.sum(10, 20);
+        int expectedRes = 30;
+        Assertions.assertEquals(expectedRes, actualRes);
+    }
+
+
+    @Disabled
     void divisionPositiveTest(){
         Assertions.assertEquals(5, mathematic.div(10, 2));
     }
 
 
 
+    @Timeout(1)
     @Test
-    void doubleSumPositiveTest(){
+    void doubleSumPositiveTest() throws InterruptedException {
+        Thread.sleep(2000);
         System.out.println("test");
         double actual = Mathematic.sumDouble(0.1, 0.7);
         double expected = 0.8;
